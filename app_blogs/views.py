d5b0ea586f1c926cb7e97a1b6410190849420832
Akhmedov76 <ahmadovj5016@gmail.com>
2024-11-06 20:00:47 +0500

    create blogs list

--- app_blogs/views.py
+++ app_blogs/views.py
@@ -2,8 +2,8 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from app_blogs.models import UsersModel
-from app_blogs.serializers import UsersSerializer
+from app_blogs.models import UsersModel, BlogsModel
+from app_blogs.serializers import UsersSerializer, BlogSerializer
 
 
 @api_view(['GET', 'POST'])
@@ -39,5 +39,42 @@ def user_detail_view(request, pk):
         response = {
             "status": True,
             "message": "User deleted successfully."
+        }
+        return Response(response, status=status.HTTP_200_OK)
+
+
+@api_view(['GET', 'POST'])
+def blog_list_create(request):
+    if request.method == 'GET':
+        blogs = BlogsModel.objects.all()
+        serializer = BlogSerializer(blogs, many=True).data
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT', 'PATCH', 'DELETE'])
+def blog_detail_update_delete(request, pk):
+    blog = BlogsModel.objects.get(pk=pk)
+    if request.method in ['PATCH', 'PUT']:
+        serializer = BlogSerializer(blog, data=request.data, partial=(request.method == 'PATCH'))
+        serializer.is_valid()
+        serializer.save()
+        response = {
+            "status": True,
+            "message": "Blog updated successfully." if request.method == 'PUT' else "Blog created successfully.",
+            "data": serializer.data
+        }
+        return Response(response, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        blog.delete()
+        response = {
+            "status": True,
+            "message": "Blog deleted successfully."
         }
         return Response(response, status=status.HTTP_200_OK)
