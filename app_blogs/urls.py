0c38735c3e39de9d6a432ec40f19444e023b7b55
Akhmedov76 <ahmadovj5016@gmail.com>
2024-11-06 19:58:23 +0500

    create urls for users

---
+++ app_blogs/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from app_blogs import views
+
+app_name = 'blogs'
+
+urlpatterns = [
+    path('', views.user_list_view, name='user_list'),
+    path('users/<int:pk>/', views.user_detail_view, name='users_detail')
+
+]
--- app_blogs/views.py
+++ app_blogs/views.py
@@ -10,7 +10,7 @@ from app_blogs.serializers import UsersSerializer
 def user_list_view(request):
     if request.method == 'GET':
         users = UsersModel.objects.all()
-        serializer = UsersSerializer(users, many=True)
+        serializer = UsersSerializer(users, many=True).data
         return Response(serializer.data)
 
     elif request.method == 'POST':
@@ -19,3 +19,25 @@ def user_list_view(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT', 'PATCH', 'DELETE'])
+def user_detail_view(request, pk):
+    user = UsersModel.objects.get(pk=pk)
+    if request.method in ['PATCH', 'PUT']:
+        serializer = UsersSerializer(user, data=request.data, partial=(request.method == 'PATCH'))
+        serializer.is_valid()
+        serializer.save()
+        response = {
+            "status": True,
+            "message": "User updated successfully." if request.method == 'PUT' else "User created successfully.",
+            "data": serializer.data
+        }
+        return Response(response, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        user.delete()
+        response = {
+            "status": True,
+            "message": "User deleted successfully."
+        }
+        return Response(response, status=status.HTTP_200_OK)
