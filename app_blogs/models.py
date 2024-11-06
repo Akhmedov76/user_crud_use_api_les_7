13d362c314fac5ed8ee6440c37452ea18c53c4e8
Akhmedov76 <ahmadovj5016@gmail.com>
2024-11-06 19:42:14 +0500

    Create usersmodel

--- app_blogs/models.py
+++ app_blogs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class UsersModel(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+        ordering = ['-id']
