3f9775b05340032b160654e297a04e907aca371a
Akhmedov76 <ahmadovj5016@gmail.com>
2024-11-06 19:43:19 +0500

    Create blogsmodel

--- app_blogs/models.py
+++ app_blogs/models.py
@@ -17,3 +17,19 @@ class UsersModel(models.Model):
         verbose_name = 'user'
         verbose_name_plural = 'users'
         ordering = ['-id']
+
+
+class BlogsModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(UsersModel, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'blog'
+        verbose_name_plural = 'blogs'
+        ordering = ['-id']
