aa9faa1c326eafcec933e0808bbd9cdaa260357d
Akhmedov76 <ahmadovj5016@gmail.com>
2024-11-06 19:51:08 +0500

    create user view

--- app_blogs/views.py
+++ app_blogs/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from app_blogs.models import UsersModel
+from app_blogs.serializers import UsersSerializer
+
+
+@api_view(['GET', 'POST'])
+def user_list_view(request):
+    if request.method == 'GET':
+        users = UsersModel.objects.all()
+        serializer = UsersSerializer(users, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = UsersSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
