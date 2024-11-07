a0c92c398038e5cd8efaea5baa6143a49f669e0d
Jasurbek <163736163+Akhmedov76@users.noreply.github.com>
2024-11-07 11:19:49 +0500

    Update urls.py

--- app_blogs/urls.py
+++ app_blogs/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('users/<int:pk>/', views.user_detail_view, name='users_detail'),
     path('blogs/', views.blog_list_create, name='blog_list'),
     path('blogs/<int:pk>/', views.blog_detail_update_delete, name='blog_detail'),
+    path('blogs/users/<int:pk>/', views.blog_detail_update_delete, name='api_user_list'),
 ]
